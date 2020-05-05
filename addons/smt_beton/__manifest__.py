23e557f868e164230fcd1280b856b53825c2326e
YusufCandraArif <cancanunyu@gmail.com>
2020-05-05 15:57:50 +0700

    update smt beton

--- addons/smt_beton/__manifest__.py
+++ addons/smt_beton/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'mrp','product','account'],
+    'depends': ['base', 'sale', 'mrp','product','account','website'],
 
     # always loaded
     'data': [
